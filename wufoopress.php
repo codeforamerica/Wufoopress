f6410afd89278cbe6656b230c80a7ea8459d7878
Abhi Nemani <abhi.nemani@gmail.com>
2011-04-07 11:46:58 -0700

    redundancy check, categories, and formatting

--- wufoopress.php
+++ wufoopress.php
@@ -1,63 +1,52 @@
 <?php
 
+    include('/Applications/MAMP/htdocs/cfa_wordpress/wp-includes/class-IXR.php');
+    $client = new IXR_Client('http://localhost/cfa_wordpress/xmlrpc.php');  
+    
+    // get Recent Posts & WufooID Custom field number
+    $client->query('metaWeblog.getRecentPosts', '', 'abhi', 'password', 1);
+    $posts = $client->getResponse();
+    $latestpost = $posts[0];
+    
+    foreach ( ($latestpost["custom_fields"]) as $meta ) {
+        $custom_fields[$meta["key"]] = $meta['value'];
+        }
+    
+    $WufooIDFilter = 'Filter1=EntryId+Is_greater_than+'.$custom_fields["WufooID"];    
+    
+          
+    // got wufoo form entries
+    
     require_once('lib/wufoo/WufooApiWrapper.php');
     $wrapper = new WufooApiWrapper('Z5TN-C4X6-B2XV-8YHB', 'codeforamerica'); //create the class
-//    echo "<br /><h2>Entries</h2>";
-//    print_r($wrapper->getEntries('x7x3q1', 'forms')); //Notice the filter
-//    echo "<br /><h2>Fields</h2>";
-//  print_r($wrapper->getFields('x7x3q1', 'forms')); //Notice the filter
-
-    $entries = $wrapper->getEntries('x7x3q1', 'forms');
+    $entries = $wrapper->getEntries('x7x3q1', 'forms', $WufooIDFilter );
     $fields = $wrapper->getFields('x7x3q1', 'forms');
-    
-    include('/Applications/MAMP/htdocs/cfa_wordpress/wp-includes/class-IXR.php');
 
-    $client = new IXR_Client('http://localhost/cfa_wordpress/xmlrpc.php');  
+    // post wufoo form entries as posts
 
-
-
-
-    $client->query('metaWeblog.getRecentPosts', '', 'abhi', 'password', 1);
-
-$posts = $client->getResponse();
-$latestpost = $posts[0];
-foreach ( ($latestpost["custom_fields"]) as $meta ) {
-    $custom_fields[$meta["key"]] = $meta['value'];
-}
-
-print_r($custom_fields["WufooID"]);
-
-
-/* foreach ($entries as $app)
-
+   foreach ($entries as $app)
     {
-        
-        
-
-   $content['title'] = "$app->Field1";
-
-         $content['description'] = "<p>".$app->Field3."</p>";
-         
-         
-         $content['mt_excerpt'] = "<p>".$app->Field366."</p>";
-
-
+        $content['title'] = "$app->Field1";
+        $content['description'] = "<p>".$app->Field3."</p>";
+        $content['mt_excerpt'] = "<p>".$app->Field366."</p>";
+        $content['mt_excerpt'] = "<p>".$app->Field366."</p>";
+        $content['custom_fields'] = array(
+            	array( 'key' => 'WufooID', 'value' => $app->EntryId )
+            	);
         $client->query('metaWeblog.newPost', '', 'abhi', 'password', $content, true);
 
         if ($client->message->faultString)
 
         {
-
             echo "Failure - ".$client->message->faultString."<br />";
-
+            
         } else {
 
             echo "Success - ".$status->text."<br />";
-
+            
         }
 
-    } */
-
+    } 
 
 
 
